5434598c14892031adfaa9859ba29556e6864b25
abhinav312 <qwertyabhipandey.798@gmail.com>
2025-03-18 16:21:29 +0530

    Sinch updated by Abhinav

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-        return "DevOps Project for Sinch Cloud Communications Pvt Ltd";
+        return "DevOps Project for Sinch Cloud Communications Pvt Ltd by Abhinav";
     }
 }
