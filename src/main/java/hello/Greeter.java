c9b88d52480875e2fa0ca6cf3a2f78d83562c981
Abhinav Pandey <abhpan@on.sinch.com>
2025-09-25 23:51:14 +0530

    n

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-        return "DevOps Project for Sinch India by Abhinav and Vipul only";
+        return "DevOps Project for Sinch India by Abhinav and Vipul";
     }
 }
