967f93506d4114598ce862b6b879040fc2693767
Abhinav Pandey <abhpan@on.sinch.com>
2025-09-26 16:19:37 +0530

    Pranjal

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-        return "DevOps Project for Sinch India by Anmol";
+        return "DevOps Project for Sinch India by Pranjal Singh ACL";
     }
 }
